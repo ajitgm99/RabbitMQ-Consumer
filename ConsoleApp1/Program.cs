cda32c3c8ce0f39b25f08279901ac9b05b9a73f1
Ajit Mhatre <ajitgm99@gmail.com>
2024-07-03 23:43:59 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "user",
+    Password = "password",
+    VirtualHost = "/"
+};
+
+var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("Registration1", false, false, false, null);
+
+var consumer=new EventingBasicConsumer(channel);
+
+consumer.Received += (model, args) =>
+{
+    var body = args.Body.ToArray();
+
+    var message=Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+};
+
+channel.BasicConsume("Registration1", false, consumer);
+Console.ReadLine();
